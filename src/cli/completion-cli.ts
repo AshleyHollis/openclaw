13f77e417e087a786fc11af9d1d0de8b13d3fa86
Ashley Hollis <AshleyHollis@users.noreply.github.com>
2026-08-23 23:05:29 +1000

    Revert "[pre-commit.ci] auto fixes from pre-commit.com hooks"
    This reverts commit 7d3718c5b4938185ff049743f7db9d03ef237c5e.

--- src/cli/completion-cli.ts
+++ src/cli/completion-cli.ts
@@ -263,7 +263,7 @@ function generateZshCompletion(program: Command): string {
 _${rootCmd}_root_completion() {
   local -a commands
   local -a options
-
+  
   _arguments -C \\
     ${generateZshArgs(program)} \\
     ${generateZshSubcmdList(program)} \\
@@ -368,7 +368,7 @@ function generateZshSubcommands(program: Command, prefix: string): string {
 ${funcName}() {
   local -a commands
   local -a options
-
+  
   _arguments -C \\
     ${generateZshArgs(cmd)} \\
     ${generateZshSubcmdList(cmd)} \\
@@ -630,7 +630,7 @@ ${optionChoiceCases}
   return `
 Register-ArgumentCompleter -Native -CommandName ${rootCmd} -ScriptBlock {
     param($wordToComplete, $commandAst, $cursorPosition)
-
+    
     $commandElements = $commandAst.CommandElements
     $commandPath = ""
     $valueOptions = ${formatPowerShellArray(rootValueOptions)}
@@ -687,7 +687,7 @@ ${commandPathUpdate}
     }
 
 ${choiceCompletion}
-
+    
     # Root command
     if ($commandPath -eq "") {
          $completions = ${formatPowerShellArray(root.completions)}
@@ -695,7 +695,7 @@ ${choiceCompletion}
             [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterName', $_)
          }
     }
-
+    
     ${rootBody}
 }
 `;
